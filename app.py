7ddbc1eb3976c0d5abe531d417d0d89d29d6a84e
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-04-13 20:30:10 +0800

    added decorator

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 import test
 
 app = Flask(__name__)
 
+
+#defining view routes
 @app.route('/')
 @app.route('/index/')
 @app.route('/index/#selectroom/')
@@ -11,9 +13,19 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/index/#selectroom/')
-def show_data():
+@app.route('/#selectroom/', methods = ["GET","POST"])
+def send_data():
 
+    #trying out flask html form data retrieval
+    if request.method == "GET":
+         selected = request.args['offices']
+         return selected
+    else:
+        return redirect('index.html/#selectroom')
+
+@app.route("/<room>")
+def show_data(room):
+    return f"<h1>{room}</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
