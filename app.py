869ac1dfd31c73810d7b06476c42db6014cb03b6
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2024-02-04 20:18:54 +0800

    updated source code

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def get_data():
     if request.method == "GET":
 
         #passing arguments to the AI based on the selected input
-        if request.args['submit'] == 'submit_office':
+        if request.args['submit'] == 'submit office':
             office = request.args['offices']
             selected = office
             direction =  main_ai(selected)#passing user input to the ai
@@ -34,14 +34,14 @@ def get_data():
             #returning the generated path/direction to the web browser
             return render_template('direction.html',direction = direction)
         
-        if request.args['submit'] == 'submit_lab':
+        if request.args['submit'] == 'submit lab':
             lab = request.args['labrooms']
             selected = lab
             direction = main_ai(selected)#passing user input to the ai
           
             #returning the generate path/direction to the web server
             return render_template('direction.html',direction = direction)
-        if request.args['submit'] == 'submit_lecture':
+        if request.args['submit'] == 'submit lecture':
             lecture_room = request.args['lecturerooms'] 
             selected = lecture_room
             direction = main_ai(selected)#passing user input to the ai
---
+++ static/functions.js
@@ -0,0 +1,5 @@
+function show()
+{
+    document.getElementById('map').style.display = "block"
+    document.getElementById('button').style.display ="none"
+}
